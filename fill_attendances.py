8ef5acc6cb4b8178d4b054aebc23dc11ce1a9847
andmichalski <andrzmichal@gmail.com>
2018-11-08 09:30:03 +0100

    Working script with searching data

--- fill_attendances.py
+++ fill_attendances.py
@@ -1,73 +1,135 @@
+import re
 import time
+from datetime import date, datetime, timedelta
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 
 LOGIN_FILE_PATH = "/home/amich/odoo_attendances/login_data.txt"
-with open(LOGIN_FILE_PATH, "r") as file:
-    data = [line.replace('\n', '').split(" ")[1] for line in file.readlines()]
-    LOGIN = data[0]
-    PASSWORD = data[1]
 
-NAME = "Andrzej"
-SURNAME = "Michalski"
-CHECK_IN = "09:00:00"
-CHECK_OUT = "16:20:00"
+class FillAttendances():
 
-driver = webdriver.Chrome()
-driver.get("https://odoo.servocode.com/web#menu_id=386&action=542")
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.wait = WebDriverWait(self.driver, 10)
+        self.short_wait = WebDriverWait(self.driver, 2)
+        self.login = None
+        self.password = None
+        self.check_in_hours = None
+        self.check_out_hours = None
+        self.name = None
+        self.surname = None
 
-login_element = driver.find_element_by_id("login")
-login_element.send_keys(LOGIN)
+    def parse_base_user_data(self):
+        with open(LOGIN_FILE_PATH, "r") as file:
+            data = [line.replace('\n', '').split(" ")[1] for line in file.readlines()]
+            self.login = data[0]
+            self.password = data[1]
+            self.check_in_hours = data[2]
+            self.check_out_hours = data[3]
 
-password_element = driver.find_element_by_id("password")
-password_element.send_keys(PASSWORD)
-password_element.send_keys(Keys.RETURN)
+        email = re.split("\.|\@", self.login)
+        self.name = email[0].title()
+        self.surname = email[1].title()
 
-wait = WebDriverWait(driver, 10)
+    def login_user(self):
+        self.driver.get("https://odoo.servocode.com/web#menu_id=386&action=542")
 
-attendance_button = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div[1]/div[11]/ul/li[1]/a/span")))
-attendance_button.click()
+        login_element = self.driver.find_element_by_id("login")
+        login_element.send_keys(self.login)
 
-def find_last_date():
-    wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[1]/div/div/div[2]"))).click()
-    wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[1]/div/input"))).send_keys(SURNAME, Keys.ENTER)
-    last_record = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/div[2]/div/div/div/table/tbody/tr[1]/td[3]")))
-    last_date = last_record.text.split(" ")[0]
-    return last_date
+        password_element = self.driver.find_element_by_id("password")
+        password_element.send_keys(self.password)
+        password_element.send_keys(Keys.RETURN)
 
-last_date = "11/07/2018"
-test_date_1 = last_date + " 09:00:00"
-test_date_2 = last_date + " 16:20:00"
+        attendance_button = self.wait.until(EC.element_to_be_clickable((
+                                                                       By.XPATH,
+                                                                       "/html/body/div[1]/div[1]/div[1]/div[11]/ul/li[1]/a/span")))
+        attendance_button.click()
 
-# Create new Attendance
-wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[2]/div[1]/div/button[1]"))).click()
-main_content = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "o_main_content")))
+    def find_last_filled_date(self):
+        self.wait.until(EC.element_to_be_clickable((By.XPATH,
+                                                    "/html/body/div[1]/div[2]/div[1]/div[1]/div/div/div[2]"))).click()
+        self.wait.until(EC.element_to_be_clickable((By.XPATH,
+                                                    "/html/body/div[1]/div[2]/div[1]/div[1]/div/input"))).send_keys(
+            self.surname, Keys.ENTER)
+        time.sleep(1)
+        last_record = self.wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/div[2]/div/div/div/table/tbody/tr[1]/td[3]")))
+        last_date = last_record.text.split(" ")[0]
+        return last_date
 
-driver.implicitly_wait(2)
+    def create_new_attendance(self, attendance_date = "11/07/2018"):
+        check_in_time = attendance_date + " " + self.check_in_hours
+        check_out_time = attendance_date + " " + self.check_out_hours
 
-check_out= driver.find_element_by_name("check_out")
-check_out.click()
-check_out.clear()
-check_out.send_keys(test_date_2)
-check_out.click()
+        try:
+            self.short_wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[2]/div[1]/div/div[1]/button[2]"))).click()
+        except TimeoutException:
+            self.wait.until(EC.element_to_be_clickable((By.XPATH,
+                                                        "/html/body/div[1]/div[2]/div[1]/div[2]/div[1]/div/button[1]"))).click()
 
-check_in = driver.find_element_by_name("check_in")
-check_in.click()
-check_in.clear()
-check_in.send_keys(test_date_1)
-check_in.click()
+        self.wait.until(EC.presence_of_all_elements_located(
+            (By.CLASS_NAME, "o_main_content")))
 
-employee_name = driver.find_element_by_css_selector("input[id*='o_field_input']")
-employee_name.click()
-employee_name_input = NAME + " " + SURNAME
-employee_name.send_keys(employee_name_input)
+        self.driver.implicitly_wait(2)
 
-time.sleep(1)
+        check_out = self.driver.find_element_by_name("check_out")
+        check_out.click()
+        check_out.clear()
+        check_out.send_keys(check_out_time)
+        check_out.click()
 
-driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/div[2]/div[1]/div/div[2]/button[1]").click()
+        check_in = self.driver.find_element_by_name("check_in")
+        check_in.click()
+        check_in.clear()
+        check_in.send_keys(check_in_time)
+        check_in.click()
 
+        employee_name = self.driver.find_element_by_css_selector(
+            "input[id*='o_field_input']")
+        employee_name.click()
+        employee_name_input = self.name + " " + self.surname
+        employee_name.send_keys(employee_name_input)
+
+        time.sleep(1)
+
+        self.driver.find_element_by_xpath(
+            "/html/body/div[1]/div[2]/div[1]/div[2]/div[1]/div/div[2]/button[1]").click()
+
+
+
+class FindWorkDays():
+
+    def find_work_days(self, last_flled_date_str="11/01/2018"):
+        today_date = datetime.now().date()
+
+        last_filled_values = last_flled_date_str.split("/")
+        last_filled_date = date(int(last_filled_values[2]),
+                                        int(last_filled_values[0]),
+                                        int(last_filled_values[1]))
+
+        begin_date = last_filled_date + timedelta(days=1)
+        dates_delta = today_date - begin_date
+
+        days = []
+        for day in range(dates_delta.days + 1):
+            temp_date = begin_date + timedelta(day)
+            if temp_date.weekday() < 5:
+                days.append(temp_date.strftime('%m/%d/%Y'))
+
+        return days
+
+
+if __name__ == "__main__":
+    fa = FillAttendances()
+    fwd = FindWorkDays()
+    fa.parse_base_user_data()
+    fa.login_user()
+    last_filled_date = fa.find_last_filled_date()
+    days = fwd.find_work_days(last_filled_date)
+    for day in days:
+        fa.create_new_attendance(day)
