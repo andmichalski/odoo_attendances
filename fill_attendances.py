e7d94fb66472453676c939f4c75658704b12acd4
Andrzej Michalski <38986141+andmichalski@users.noreply.github.com>
2018-11-08 10:30:35 +0100

    Update fill_attendances.py

--- fill_attendances.py
+++ fill_attendances.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-LOGIN_FILE_PATH = "/home/amich/odoo_attendances/login_data.txt"
+LOGIN_FILE_PATH = "/home/amich/odoo_attendances/login.txt"
 
 
 class FillAttendances():
